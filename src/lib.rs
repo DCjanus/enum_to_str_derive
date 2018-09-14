c747a4a55ccc43dd2c329e5b84deeef58d50d32b
DCjanus <DCjanus@dcjanus.com>
2018-09-14 11:52:25 +0800

    update CI config and rustfmt config

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,10 @@ pub fn enum_to_str(input: TokenStream) -> TokenStream {
     let derive_input = syn::parse::<DeriveInput>(input).unwrap();
     let target_enum = Enum::parse(&derive_input);
     let enum_name = syn::parse_str::<Ident>(target_enum.name.as_ref()).unwrap();
-    let match_content = target_enum.variants.iter().map(|x| each_variant(x, &enum_name));
+    let match_content = target_enum
+        .variants
+        .iter()
+        .map(|x| each_variant(x, &enum_name));
 
     quote!(
         impl #enum_name {
@@ -44,8 +47,12 @@ fn each_variant(source: &Variant, enum_name: &Ident) -> impl quote::ToTokens {
 
 fn variant_value(source: &Variant) -> impl quote::ToTokens {
     if let Some(value_attribute) = target_attributes(source).find(|x| x.name == "value") {
-        let value = lit_str_value(value_attribute.values.first().expect("string value must be set"))
-            .expect("value must be string");
+        let value = lit_str_value(
+            value_attribute
+                .values
+                .first()
+                .expect("string value must be set"),
+        ).expect("value must be string");
         return quote!(#value);
     }
 
